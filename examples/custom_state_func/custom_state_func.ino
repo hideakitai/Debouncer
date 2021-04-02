24b27346791937352d820823d875a5ffe24f9715
Hideaki Tai <hideaki.tai@gmail.com>
2021-04-02 10:24:14 +0900

    change state function to lambda

--- examples/custom_state_func/custom_state_func.ino
+++ examples/custom_state_func/custom_state_func.ino
@@ -3,24 +3,28 @@
 int debounce_duration_ms = 50;
 Debouncer debouncer(debounce_duration_ms);
 
-// you can use any state function which returns int (and no args)
-int customStateFunc() {
-    int b0 = digitalRead(2);
-    int b1 = digitalRead(3);
-    int value = b0 | (b1 << 1);
-    return value;
-}
-
 void setup() {
     Serial.begin(115200);
     pinMode(2, INPUT_PULLUP);
     pinMode(3, INPUT_PULLUP);
     delay(2000);
 
-    // set your own state function which returns bool with no args instead of `digitalRead(pin)`
-    // debouncer.stateFunc(customStateFunc);
-    // you can also set default value
-    debouncer.stateFunc(customStateFunc, 3);
+    // you can use any state function which returns int (and no args)
+    // instead of `digitalRead(pin)`
+    // debouncer.stateFunc([](){
+    //     int b0 = digitalRead(2);
+    //     int b1 = digitalRead(3);
+    //     int value = b0 | (b1 << 1);
+    //     return value;
+    // });
+
+    // you can also set default value for custom state function
+    debouncer.stateFunc(3, []() {
+        int b0 = digitalRead(2);
+        int b1 = digitalRead(3);
+        int value = b0 | (b1 << 1);
+        return value;
+    });
 
     // if you set your state function, you need to set your callback to CHANGED
     // if you don't specify the Edge, the callback is set to Edge::CHANGE automatically
